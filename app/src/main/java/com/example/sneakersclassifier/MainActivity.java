1d99ebe2da0fe008c7bd1bec22032c803940f366
CristaudoGiuseppe <gcristaudo98@gmail.com>
2022-02-15 17:05:21 +0100

    code enhanced

--- app/src/main/java/com/example/sneakersclassifier/MainActivity.java
+++ app/src/main/java/com/example/sneakersclassifier/MainActivity.java
@@ -6,17 +6,17 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.content.res.AssetFileDescriptor;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -24,25 +24,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import org.tensorflow.lite.DataType;
-import org.tensorflow.lite.Interpreter;
-import org.tensorflow.lite.support.common.FileUtil;
-import org.tensorflow.lite.support.common.TensorOperator;
-import org.tensorflow.lite.support.common.TensorProcessor;
-import org.tensorflow.lite.support.common.ops.NormalizeOp;
-import org.tensorflow.lite.support.image.ImageProcessor;
-import org.tensorflow.lite.support.image.TensorImage;
-import org.tensorflow.lite.support.image.ops.ResizeOp;
-import org.tensorflow.lite.support.image.ops.ResizeWithCropOrPadOp;
-import org.tensorflow.lite.support.label.TensorLabel;
-import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
@@ -56,29 +39,28 @@ public class MainActivity extends AppCompatActivity {
     private static final int PERMISSION_REQUEST_CODE = 200;
 
 
-    private Bitmap bitmap;
-    protected Interpreter tflite;
-    private TensorImage inputImageBuffer;
-    private  int imageSizeX;
-    private  int imageSizeY;
-    private  TensorBuffer outputProbabilityBuffer;
-    private  TensorProcessor probabilityProcessor;
-    private static final float IMAGE_MEAN = 0.0f;
-    private static final float IMAGE_STD = 1.0f;
-    private static final float PROBABILITY_MEAN = 0.0f;
-    private static final float PROBABILITY_STD = 255.0f;
-
-
     // define XML elements
     ImageView imageView;
     Button classify_button;
     Button take_a_picture;
     TextView results;
-
+    TextView link_result;
     Uri imageuri;
-    private List<String> labels;
+
+    private Bitmap bitmap;
     private ImageClassifier imageClassifier;
-    private boolean imageLoaded = false;
+    private boolean imageLoaded;
+    private Map<String, Float> predictions;
+
+    // link
+    private String link_aj11 = "https://restocks.net/it/c/sneakers/air-jordan/Air-Jordan-11";
+    private String link_blazer = "https://stockx.com/search?s=blazer%20mid";
+    private String link_am1 = "https://restocks.net/it/c/sneakers/nike/air-max/air-max-1";
+    private String link_yeezy_700 = "https://restocks.net/it/c/sneakers/adidas/yeezy/700/v1";
+    private String link_yeezy_350 = "https://restocks.net/it/c/sneakers/adidas/yeezy/350/v2";
+    private String link_aj4 = "https://restocks.net/it/c/sneakers/air-jordan/air-jordan-4";
+    private String link_aj1 = "https://restocks.net/it/c/sneakers/air-jordan/air-jordan-1/high";
+    private String link_yeezy_slide = "https://restocks.net/it/c/sneakers/adidas/yeezy/slide";
 
 
 
@@ -92,23 +74,16 @@ public class MainActivity extends AppCompatActivity {
             requestPermission();
         }
 
+        imageLoaded = false;
+
         try {
             imageClassifier = new ImageClassifier(this);
         } catch (IOException e) {
             Log.e("Image Classifier Error", "ERROR: " + e);
         }
 
-        // import tensorflow lite model
-        try {
-            tflite = new Interpreter(loadmodelfile(MainActivity.this));
-        } catch (IOException e) {
-            Log.e("Tensorflow Error", "ERROR: " + e);
-        }
-
-        // initalizing ui elements
         initializeUIElements();
     }
-
 
 
     private void initializeUIElements() {
@@ -116,7 +91,8 @@ public class MainActivity extends AppCompatActivity {
         imageView = (ImageView)findViewById(R.id.imageView);
         classify_button = (Button)findViewById(R.id.classify);
         take_a_picture = (Button)findViewById(R.id.take_a_picture);
-        results =(TextView)findViewById(R.id.results);
+        results = (TextView)findViewById(R.id.results);
+        link_result = (TextView)findViewById(R.id.link_text);
 
         // select an image from gallery
         imageView.setOnClickListener(new View.OnClickListener() {
@@ -126,6 +102,7 @@ public class MainActivity extends AppCompatActivity {
                 intent.setType("image/*");
                 intent.setAction(Intent.ACTION_GET_CONTENT);
                 startActivityForResult(Intent.createChooser(intent,"Select image"),SELECT_IMAGE_REQUEST_CODE);
+                imageLoaded = true;
             }
         });
 
@@ -141,29 +118,10 @@ public class MainActivity extends AppCompatActivity {
         classify_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                try {
-                    int imageTensorIndex = 0;
-                    int[] imageShape = tflite.getInputTensor(imageTensorIndex).shape(); // {1, height, width, 3}
-                    imageSizeY = imageShape[1];
-                    imageSizeX = imageShape[2];
-                    DataType imageDataType = tflite.getInputTensor(imageTensorIndex).dataType();
-
-                    int probabilityTensorIndex = 0;
-                    int[] probabilityShape =
-                            tflite.getOutputTensor(probabilityTensorIndex).shape(); // {1, NUM_CLASSES}
-                    DataType probabilityDataType = tflite.getOutputTensor(probabilityTensorIndex).dataType();
-
-                    inputImageBuffer = new TensorImage(imageDataType);
-                    outputProbabilityBuffer = TensorBuffer.createFixedSize(probabilityShape, probabilityDataType);
-                    probabilityProcessor = new TensorProcessor.Builder().add(getPostprocessNormalizeOp()).build();
-
-                    inputImageBuffer = loadImage(bitmap);
-
-                    tflite.run(inputImageBuffer.getBuffer(), outputProbabilityBuffer.getBuffer().rewind());
-                    showresult();
-                } catch (Exception e) {
-                    Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();
-                    e.printStackTrace();
+                if(imageLoaded) {
+                    showResult();
+                } else {
+                    Toast.makeText(getApplicationContext(), "Please select an image", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -178,14 +136,13 @@ public class MainActivity extends AppCompatActivity {
     private boolean checkPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
                 != PackageManager.PERMISSION_GRANTED) {
-            // Permission is not granted
+            // permission is not granted
             return false;
         }
         return true;
     }
 
     private void requestPermission() {
-
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.CAMERA},
                 PERMISSION_REQUEST_CODE);
@@ -207,8 +164,6 @@ public class MainActivity extends AppCompatActivity {
             case PERMISSION_REQUEST_CODE:
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     Toast.makeText(getApplicationContext(), "Permission Granted", Toast.LENGTH_SHORT).show();
-
-                    // main logic
                 } else {
                     Toast.makeText(getApplicationContext(), "Permission Denied", Toast.LENGTH_SHORT).show();
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
@@ -230,93 +185,73 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        //possibilità di fare switch
-        if(requestCode==SELECT_IMAGE_REQUEST_CODE && resultCode==RESULT_OK && data!=null) {
-            imageuri = data.getData();
-            try {
-                bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageuri);
-                imageView.setImageBitmap(bitmap);
-            } catch (IOException e) {
-                e.printStackTrace();
+
+        if(resultCode == RESULT_OK && data != null) {
+            switch (requestCode) { // switch used for futureproof
+                case SELECT_IMAGE_REQUEST_CODE:
+                    imageuri = data.getData();
+                    try {
+                        bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageuri);
+                        imageView.setImageBitmap(bitmap);
+                        predictions = imageClassifier.recognizeImage(bitmap, 0);
+                    } catch (IOException e) {
+                        Log.e("Image Classifier", "ERROR: " + e);
+                    }
+                break;
+                case CAMERA_REQUEST_CODE:
+                    try {
+                        Bitmap photo = (Bitmap) Objects.requireNonNull(Objects.requireNonNull(data).getExtras()).get("data");
+                        imageView.setImageBitmap(photo);
+                        predictions = imageClassifier.recognizeImage(photo, 0);
+                    } catch (Exception e) {
+                        Log.e("Image Classifier", "ERROR: " + e);
+                    }
+                break;
+            }
+        }
+    }
+
+    private void showResult() {
+        String buy_link = "";
+        float maxValueInMap = (Collections.max(predictions.values()));
+        for ( String key : predictions.keySet() ) {
+            if (predictions.get(key) == maxValueInMap) {
+                float prob = predictions.get(key) * 100;
+                String result_string = "The sneakers is a " + key + " with " + prob + "% probability";
+                switch (key){
+                    case "Air Jordan 11":
+                        buy_link = link_aj11;
+                    break;
+                    case "Nike Blazer Mid":
+                        buy_link = link_blazer;
+                    break;
+                    case "Nike Air Max 1":
+                        buy_link = link_am1;
+                    break;
+                    case "Yeezy Boost 700":
+                        buy_link = link_yeezy_700;
+                    break;
+                    case "Yeezy Boost 350 V2":
+                        buy_link = link_yeezy_350;
+                    break;
+                    case "Air Jordan 4":
+                        buy_link = link_aj4;
+                    break;
+                    case "Air Jordan 1 High":
+                        buy_link = link_aj1;
+                    break;
+                    case "Yeezy Slide":
+                        buy_link = link_yeezy_slide;
+                    break;
+                }
+                results.setText(result_string);
+                link_result.setText("Buy one at " + buy_link);
             }
         }
 
-        if (requestCode == CAMERA_REQUEST_CODE) {
-            // getting bitmap of the image
-            Bitmap photo = (Bitmap) Objects.requireNonNull(Objects.requireNonNull(data).getExtras()).get("data");
-            // displaying this bitmap in imageview
-            imageView.setImageBitmap(photo);
-
-            // pass this bitmap to classifier to make prediction
-            List<ImageClassifier.Recognition> predicitons = imageClassifier.recognizeImage(
-                    photo, 0);
-
-            // creating a list of string to display in list view
-            final List<String> predicitonsList = new ArrayList<>();
-            for (ImageClassifier.Recognition recog : predicitons) {
-                predicitonsList.add(recog.getName() + "  ::::::::::  " + recog.getConfidence());
-            }
-
-            results.setText(predicitonsList.get(0));
-
-        }
-
-
-    }
-
-    private MappedByteBuffer loadmodelfile(Activity activity) throws IOException {
-        AssetFileDescriptor fileDescriptor=activity.getAssets().openFd("model.tflite");
-        FileInputStream inputStream=new FileInputStream(fileDescriptor.getFileDescriptor());
-        FileChannel fileChannel=inputStream.getChannel();
-        long startoffset = fileDescriptor.getStartOffset();
-        long declaredLength=fileDescriptor.getDeclaredLength();
-        return fileChannel.map(FileChannel.MapMode.READ_ONLY,startoffset,declaredLength);
-    }
-
-    private TensorOperator getPreprocessNormalizeOp() {
-        return new NormalizeOp(IMAGE_MEAN, IMAGE_STD);
-    }
-
-    private TensorOperator getPostprocessNormalizeOp(){
-        return new NormalizeOp(PROBABILITY_MEAN, PROBABILITY_STD);
-    }
-
-    private TensorImage loadImage(final Bitmap bitmap) {
-        // Loads bitmap into a TensorImage.
-        inputImageBuffer.load(bitmap);
-
-        // Creates processor for the TensorImage.
-        int cropSize = Math.min(bitmap.getWidth(), bitmap.getHeight());
-        // TODO(b/143564309): Fuse ops inside ImageProcessor.
-        ImageProcessor imageProcessor =
-                new ImageProcessor.Builder()
-                        .add(new ResizeWithCropOrPadOp(cropSize, cropSize))
-                        .add(new ResizeOp(imageSizeX, imageSizeY, ResizeOp.ResizeMethod.NEAREST_NEIGHBOR))
-                        .add(getPreprocessNormalizeOp())
-                        .build();
-        return imageProcessor.process(inputImageBuffer);
-    }
-
-    private void showresult() {
-        try {
-            labels = FileUtil.loadLabels(MainActivity.this, "labels.txt");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        Map<String, Float> labeledProbability =
-                new TensorLabel(labels, probabilityProcessor.process(outputProbabilityBuffer))
-                        .getMapWithFloatValue();
-        float maxValueInMap = (Collections.max(labeledProbability.values()));
-        for ( String key : labeledProbability.keySet() ) {
-            if (labeledProbability.get(key) == maxValueInMap) {
-                results.setText(key);
-            }
-
-        }
     }
 }
 
